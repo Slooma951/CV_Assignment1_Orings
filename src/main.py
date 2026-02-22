29ba232b6b4ab1a3b3341a639c322d89ff800c35
salem Elatrash <b00149614@mytudublin.ie>
2026-02-22 20:25:53 +0000

    Manual grayscale + histogram + automatic threshold implemented

--- src/main.py
+++ src/main.py
@@ -29,9 +29,43 @@ def main():
 
         start_time = time.time()
 
-        # PROCESSING PIPELINE WILL GO HERE
+        #GRAYSCALE
+        gray = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
 
-        result = "TEST"
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                b = img[i, j, 0]
+                g = img[i, j, 1]
+                r = img[i, j, 2]
+                gray[i, j] = int((int(r) + int(g) + int(b)) / 3)
+
+        # HISTOGRAM 
+        hist = np.zeros(256)
+
+        for i in range(gray.shape[0]):
+            for j in range(gray.shape[1]):
+                intensity = gray[i, j]
+                hist[intensity] += 1
+
+        #AUTOMATIC THRESHOLD (Two-Peak Method)
+        peak1 = np.argmax(hist[:128])
+        peak2 = np.argmax(hist[128:]) + 128
+        threshold = int((peak1 + peak2) / 2)
+
+        #APPLY THRESHOLD
+        binary = np.zeros_like(gray)
+
+        for i in range(gray.shape[0]):
+            for j in range(gray.shape[1]):
+                if gray[i, j] > threshold:
+                    binary[i, j] = 255
+                else:
+                    binary[i, j] = 0
+
+        
+        cv2.imwrite(os.path.join(output_folder, "binary_" + filename), binary)
+
+        result = "THRESHOLD DONE"
 
         end_time = time.time()
         processing_time = end_time - start_time
